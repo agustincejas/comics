188f35e5a099676a17bfb09459db9a881903894f
Agustín Cejas <agustindcejas@gmail.com>
2020-09-20 18:44:37 -0300

    feature: added environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  marvelApi: 'https://gateway.marvel.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  marvelApi: 'https://gateway.marvel.com/',
+  apiKey: '5045e36b4365eaf672629e0c6a59c076'
 };
 
 /*

7c228e52cc5f3d53f94b76f4992747c3c87326c9
Agustín Cejas <agustindcejas@gmail.com>
2020-09-26 21:23:58 -0300

    feat(CharacterDetail): display comics as a grid.

--- src/app/shared/components/comic-detail/comic-detail.component.spec.ts
+++ src/app/shared/components/comic-detail/comic-detail.component.spec.ts
@@ -1,10 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IComic } from '@comics-core/models/comic';
 
 import { ComicDetailComponent } from './comic-detail.component';
 
 describe('ComicDetailComponent', () => {
   let component: ComicDetailComponent;
   let fixture: ComponentFixture<ComicDetailComponent>;
+  const comicInput: IComic = {
+    id: 3537,
+    name: 'Amazing Spider-Man (1999) #529',
+    thumbnail: 'http://i.annihil.us/u/prod/marvel/i/mg/5/d0/4bc3291a4b067/landscape_xlarge.jpg',
+    url: 'spiderUrl'
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,6 +23,7 @@ describe('ComicDetailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ComicDetailComponent);
     component = fixture.componentInstance;
+    component.comic = comicInput;
     fixture.detectChanges();
   });
 

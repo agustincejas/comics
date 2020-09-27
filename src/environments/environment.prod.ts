62b97c0df65dcd9e40d954ae5d2823aa3fa942cc
Agustín Cejas <agustindcejas@gmail.com>
2020-09-27 20:04:26 -0300

    refactor(environment): added apiKey to prod environment to test deploy.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  marvelApi: 'https://gateway.marvel.com/'
+  marvelApi: 'https://gateway.marvel.com/',
+  apiKey: '5045e36b4365eaf672629e0c6a59c076'
 };

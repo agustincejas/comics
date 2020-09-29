357c2ef1981841a723f57b44b44d736603ea598b
Agustín Cejas <agustindcejas@gmail.com>
2020-09-29 03:16:26 -0300

    feat(README): edited README with instructions for app configuration.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   marvelApi: 'https://gateway.marvel.com/',
-  apiKey: '5045e36b4365eaf672629e0c6a59c076'
+  apiKey: '' //Fill with key
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   marvelApi: 'https://gateway.marvel.com/',
-  apiKey: '5045e36b4365eaf672629e0c6a59c076'
+  apiKey: '' //Fill with key
 };
 
 /*
